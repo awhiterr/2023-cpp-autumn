d5c07419effe37a1d24993a8b651604620165f02
An Weiß <st122316@student.spbu.ru>
2023-09-19 23:42:10 +0300

    Homework 1 code update

--- 2023.09.10-Homework-1/task02.cpp
+++ 2023.09.10-Homework-1/task02.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 int main(int argc, char* argv[]) {
-    int d = 0;
-    int N = 0;
-    int s = 0;
-    int n = 0;
-    std::cin >> N;
-    d = (abs(N - 1)) / (N - 1);
-    n = ((N - 1) / d) + 1;
-    s = (1 + N) * n / 2;
-    std::cout << s;
-    return EXIT_SUCCESS;
+	int d = 0;
+	int c = 0;
+	int s = 0;
+	int n = 0;
+	std::cin >> c;
+	d = (abs(c - 1)) / (c - 1);
+	n = ((c - 1) / d) + 1;
+	s = (1 + c) * n / 2;
+	std::cout << s;
+	return EXIT_SUCCESS;
 }
