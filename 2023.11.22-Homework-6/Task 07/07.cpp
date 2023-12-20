9b1d5c887c5242c1aacd850102a2fd9f371ee9ec
Anastassija Belaja <st122316@student.spbu.ru>
2023-12-21 01:39:34 +0300

    Homework 6 code update

--- 2023.11.22-Homework-6/Task 07/07.cpp
+++ 2023.11.22-Homework-6/Task 07/07.cpp
@@ -23,10 +23,67 @@ void hanoy(int count, int from = 1, int to = 3)
 	hanoy(count - 1, res, to);
 }
 
+void swap(int& a, int& b)
+{
+	int c = a;
+	a = b;
+	b = c;
+}
+
+int top(int a)
+{
+	if (n[a] > 0)
+	{
+		return d[a][n[a] - 1];
+	} 
+	else
+	{
+		return 1;
+	}
+}
+
 void sort()
 {
-	
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int tower = 1;
+
+
+	while (n[a] > 0)
+	{
+		int k = top(a);
+		while (top(c) < k)
+		{
+			int f = 0; //press f to pay respect
+			for (int i = 0; i < n[b]; ++i)
+			{
+				if (d[b][i] > top(c))
+				{
+					f++;
+				}
+			}
+			hanoy(n[b] - f, b, c);
+			swap(b, c);
+		}
+		shift(a, c);
+	}
+
+	while (n[c] > 0)
+	{
+		int f = 0;
+		for (int j = 0; j < n[b]; ++j)
+		{
+			if (d[b][j] > top(c))
+			{
+				f++;
+			}
+		}
+		hanoy(n[b] - f, b, c);
+		swap(b, c);
+	}
 }
+
 
 int main(int argc, char* argv[])
 {
